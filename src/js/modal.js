7b7979ff188ad7aad25e1538873c4a52ce5dfcc5
Gromov Danil <105980448+Xenob1na@users.noreply.github.com>
2023-07-05 21:38:30 +0700

    Section Support pratner + Section Features + animation

--- src/js/modal.js
+++ src/js/modal.js
@@ -7,17 +7,21 @@ const LoginBtn = document.getElementById('LoginBtn')
 
 SignUpBtn.onclick = function() {
     ModalLSignUp.style.display = 'block'
+    document.body.classList.toggle('no-scroll')
     console.log('..')
 };
         
 LoginBtn.onclick = function() {
     ModalLogin.style.display = 'block'
+    document.body.classList.toggle('no-scroll')
     console.log('..')
 };
         
 BtnClose.onclick = function() {
     ModalLSignUp.style.display = 'none'
+    document.body.classList.toggle('no-scroll')
 }
 BtnCloseLogin.onclick = function() {
     ModalLogin.style.display = 'none'
+    document.body.classList.toggle('no-scroll')
 }
